ea6afcccb3a7dcc032351a063192d80b753a9777
Ubuntu <ubuntu@ip-172-31-38-101.us-west-2.compute.internal>
2017-10-10 18:54:44 +0000

    Included persistant session variable. Impact of maintaining cache needs to be addressed. ie Bradd Pitt or other invalid datafields being persistant.

--- frontend/html/chatbox.php
+++ frontend/html/chatbox.php
@@ -3,11 +3,15 @@
  * @author kevin.r.jesse@gmail.com
  */
 
-#session_start();
+session_start();
 #ini_set('display_errors', 1);
 #ini_set('display_startup_errors', 1);
 #error_reporting(E_ALL);
-$UUID = uniqid();
+if (!session_id()) {
+    session_id(uniqid());
+}
+
+$UUID = session_id();
 ?>
 
 <html>

948f62ac75c7d9f156c5e4e4b98898fe87eb00dd
Austin C <auzbuzzard@gmail.com>
2017-12-20 19:28:38 +0800

    Require survey questions to be filled out. Fixed template.xml grammar.

--- frontend/html/survey.php
+++ frontend/html/survey.php
@@ -25,7 +25,7 @@
         <input type="hidden" name="cryptID" value="<?php echo @crypt($_GET["id"]) ?>">
         <fieldset class="section" name="turkID">
             <label for="turkid-input">Your Turk ID:</label>
-            <input type="text" name="turkID" id="turkid-input">
+            <input type="text" name="turkID" id="turkid-input" required>
         </fieldset>
         <fieldset class="section" id="part-1">
             <h1>Part 1</h1>
@@ -75,7 +75,7 @@
                 echo '</div>';
 
                 echo '<h2>Your rating:</h2>';
-                echo '<input type="radio" name="rating-q' . ($i + 1) . '" id="rating-q' . ($i + 1) . '-input-1" value="1">';
+                echo '<input type="radio" name="rating-q' . ($i + 1) . '" id="rating-q' . ($i + 1) . '-input-1" value="1" required>';
                 echo '<label for="rating-q' . ($i + 1) . '-input-1">Not Appropriate</label>';
                 echo '<input type="radio" name="rating-q' . ($i + 1) . '" id="rating-q' . ($i + 1) . '-input-2" value="2">';
                 echo '<label for="rating-q' . ($i + 1) . '-input-2">Interpretable</label>';
@@ -93,13 +93,15 @@
 
             <fieldset class="question" name="survey-q1">
                 <h2><label>Have you talked to a chatbot before?</label></h2>
-                <input type="radio" name="survey-q1" id="survey-q1-input-1" value="1"><label for="survey-q1-input-1">Yes</label>
+                <input type="radio" name="survey-q1" id="survey-q1-input-1" value="1" required><label
+                        for="survey-q1-input-1">Yes</label>
                 <input type="radio" name="survey-q1" id="survey-q1-input-2" value="0"><label for="survey-q1-input-2">No</label>
             </fieldset>
 
             <fieldset class="question survey" name="survey-q2">
                 <h2><label>How engaged you feel during the conversation?</label></h2>
-                <input type="radio" name="survey-q2" id="survey-q2-input-1" value="1"><label for="survey-q2-input-1">1 (None)</label>
+                <input type="radio" name="survey-q2" id="survey-q2-input-1" value="1" required><label
+                        for="survey-q2-input-1">1 (None)</label>
                 <input type="radio" name="survey-q2" id="survey-q2-input-2" value="2"><label for="survey-q2-input-2">2</label>
                 <input type="radio" name="survey-q2" id="survey-q2-input-3" value="3"><label for="survey-q2-input-3">3</label>
                 <input type="radio" name="survey-q2" id="survey-q2-input-4" value="4"><label for="survey-q2-input-4">4</label>
@@ -108,9 +110,23 @@
 
             <fieldset class="question survey" name="survey-q3">
                 <h2><label for="survey-q3-input-1">What do you like and not like about the chatbot, and any questions and suggestions?</label></h2>
-                <textarea name="survey-q3" id="survey-q3-input-1"></textarea>
-                </div>
+                <textarea name="survey-q3" id="survey-q3-input-1" required></textarea>
             </fieldset>
+
+            <fieldset class="question survey" name="survey-q4">
+                <h2><label>Do you like the recommended movie?</label></h2>
+                <input type="radio" name="survey-q4" id="survey-q4-input-1" value="1" required><label
+                        for="survey-q2-input-1">1 (No)</label>
+                <input type="radio" name="survey-q4" id="survey-q4-input-2" value="2"><label
+                        for="survey-q2-input-2">2</label>
+                <input type="radio" name="survey-q4" id="survey-q4-input-3" value="3"><label
+                        for="survey-q2-input-3">3</label>
+                <input type="radio" name="survey-q4" id="survey-q4-input-4" value="4"><label
+                        for="survey-q2-input-4">4</label>
+                <input type="radio" name="survey-q4" id="survey-q4-input-5" value="5"><label
+                        for="survey-q2-input-5">5 (Very)</label>
+            </fieldset>
+
             <input type="submit" name="submit-survey" value="Submit">
     </form>
 </section>
